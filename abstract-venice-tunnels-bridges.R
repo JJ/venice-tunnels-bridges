fef3339c481003ca34f2a5a112d6576389ea6de7
JJ Merelo <jjmerelo@gmail.com>
2025-07-02 18:14:27 +0200

    Generate poster

--- abstract-venice-tunnels-bridges.R
+++ abstract-venice-tunnels-bridges.R
@@ -2,8 +2,14 @@
 library(igraph)
 load("venice.graph.undirected.Rdata")
 E(venice.graph.undirected)[ E(venice.graph.undirected)$sotoportego != FALSE ]$color <- "blue"
+V(venice.graph.undirected)$betweenness <- betweenness(venice.graph.undirected, directed=F)
+E(venice.graph.undirected)$betweenness <- edge_betweenness(venice.graph.undirected, directed=F)
 par(mar=c(0,0,0,0)+.1)
-plot(venice.graph.undirected,vertex.label=NA,vertex.shape="none", fig.pos="h!tb", edge.width=1)
+plot(venice.graph.undirected,vertex.label=NA, edge.width=1, vertex.shape="none",
+     edge.color=E(venice.graph.undirected)$color,
+     edge.width=E(venice.graph.undirected)$betweenness/200,
+     edge.arrow.size=0, vertex.size=0,
+     vertex.frame.color=NA)
 graph.edges <- E(venice.graph.undirected)
 number.of.edges <- length(graph.edges)
 percent.bridges <- round(length(graph.edges[graph.edges$bridge == T])*100/number.of.edges,2)
